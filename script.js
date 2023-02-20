0a02848836ce8757641613ce68a8e4e344ee5d81
yleniaocchi <occhiylenia.mingh@gmail.com>
2023-02-20 14:07:45 +0100

    final site 1.0

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert ("ciao") 
+$( () => {
+    $("#menu-button").on("click", function () {
+      $("#menu, #menu-button").toggleClass("is-active");
+    });
+  });
+
+  var elem = document.querySelector('.main-carousel');
+  var flkty = new Flickity( elem, {
+    cellAlign: 'left',
+    contain: true
+  });
+  var flkty = new Flickity( '.main-carousel', {
+  });
