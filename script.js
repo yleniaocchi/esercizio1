adc43c78335ce07d7cb37c1ba46c6fe0fc686104
yleniaocchi <occhiylenia.mingh@gmail.com>
2023-02-28 10:56:36 +0100

    off-canvas

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-$( () => {
-    $("#menu-button").on("click", function () {
-      $("#menu, #menu-button").toggleClass("is-active");
-    });
+$(() => {
+  $("#menu-button").on("click", function () {
+    $("#menu, #menu-button").toggleClass("is-active");
   });
+});
 
-  var elem = document.querySelector('.main-carousel');
-  var flkty = new Flickity( elem, {
-    cellAlign: 'left',
-    contain: true
-  });
-  var flkty = new Flickity( '.main-carousel', {
-  });
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+  cellAlign: 'left',
+  contain: true
+});
+var flkty = new Flickity('.main-carousel', {
+});
+
+
 
