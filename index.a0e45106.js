221bda759a710eed02883378721df5efe384f69c
yleniaocchi <occhiylenia.mingh@gmail.com>
2023-03-01 16:45:40 +0100

    Updates

---
+++ index.a0e45106.js
@@ -0,0 +1,2 @@
+window.onload=()=>{let e=document.getElementById("autotext"),t="Hi! This is my portfolio!",n=0;!function l(){n<t.length&&(e.innerHTML+=t.charAt(n),n++,setTimeout(l,60))}()},$((()=>{$("#menu-button").on("click",(function(){$("#menu, #menu-button").toggleClass("is-active")}))}));var elem=document.querySelector(".main-carousel"),flkty=new Flickity(elem,{cellAlign:"left",contain:!0});flkty=new Flickity(".main-carousel",{});
+//# sourceMappingURL=index.a0e45106.js.map
