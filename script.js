3d3dcee216fca0218d9cf198857886b6474c84f9
yleniaocchi <occhiylenia.mingh@gmail.com>
2023-03-01 16:44:44 +0100

    final

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+window.onload = () => {
+  let autotext = document.getElementById("autotext")
+  let text = "Hi! This is my portfolio!"
+  let i=0
+
+  function typer() {
+    if (i<text.length) {
+      autotext.innerHTML += text.charAt(i)
+      i++
+      setTimeout(typer, 60)
+    }
+  }
+  typer()
+}
+
 $(() => {
   $("#menu-button").on("click", function () {
     $("#menu, #menu-button").toggleClass("is-active");
@@ -11,6 +26,8 @@ var flkty = new Flickity(elem, {
 });
 var flkty = new Flickity('.main-carousel', {
 });
+
+
 
 
 
