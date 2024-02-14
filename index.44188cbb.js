76f98e01fca1b27b6c5a247ad56c80b67a0decc2
yleniaocchi <occhiylenia.mingh@gmail.com>
2024-02-14 01:15:07 +0100

    Updates

---
+++ index.44188cbb.js
@@ -0,0 +1,2 @@
+window.onload=()=>{let t=document.getElementById("autotext"),n="Hi! This is my portfolio!",e=0;!function o(){e<n.length&&(t.innerHTML+=n.charAt(e),e++,setTimeout(o,60))}()},$(()=>{$("#menu-button").on("click",function(){$("#menu, #menu-button").toggleClass("is-active")})});
+//# sourceMappingURL=index.44188cbb.js.map
